525ce8b0dae18f0d5b71ad6ee2607eb27a3c9a3a
Daniel Svensson <dsvensson@gmail.com>
2016-11-11 09:27:06 +0100

    Break out argument collection in genipc.

--- genipc/functions.go
+++ genipc/functions.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 type Arg struct {
 	Name        string
 	Type        string
@@ -19,52 +15,54 @@ type Function struct {
 	ReturnType string
 }
 
+func collectArguments(arguments []XmlArgument) []Arg {
+	var result []Arg
+
+	for _, arg := range arguments {
+		var argType string
+		var xmmsType string
+		switch arg.Type[0] {
+		case "enum-value":
+			argType = "int"
+			xmmsType = "XmmsInt"
+		case "int":
+			argType = "int"
+			xmmsType = "XmmsInt"
+		case "string":
+			argType = "string"
+			xmmsType = "XmmsString"
+		case "binary":
+			argType = "XmmsValue"
+		case "list":
+			argType = "XmmsList" // TODO: Convert to array or vararg
+		case "dictionary":
+			argType = "XmmsDict"
+		case "collection":
+			argType = "XmmsValue" // TODO: Implement Collections
+		default:
+			panic("Unexpected type")
+		}
+		result = append(result, Arg{
+			Name:        toCamelCase(arg.Name, false),
+			Type:        argType,
+			XmmsType:    xmmsType,
+			HasXmmsType: len(xmmsType) > 0,
+		})
+	}
+
+	return result
+}
+
 func collectFunctions(objects []XmlObject, offset int) []Function {
 	var functions []Function
 	for objectId, obj := range objects {
-		for commandId, meth := range obj.Methods {
-			var args []Arg
-			skip := false
-			for _, arg := range meth.Arguments {
-				var argType string
-				var xmmsType string
-				switch arg.Type[0] {
-				case "enum-value":
-					argType = "int"
-					xmmsType = "XmmsInt"
-				case "int":
-					argType = "int"
-					xmmsType = "XmmsInt"
-				case "string":
-					argType = "string"
-					xmmsType = "XmmsString"
-				case "binary":
-					argType = "XmmsValue"
-				case "list":
-					argType = "XmmsList" // TODO: Convert to array or vararg
-				case "dictionary":
-					argType = "XmmsDict"
-				case "collection":
-					argType = "XmmsValue" // TODO: Implement Collections
-				default:
-					fmt.Println(arg.Type[0])
-				}
-				args = append(args, Arg{
-					Name:        toCamelCase(arg.Name, false),
-					Type:        argType,
-					XmmsType:    xmmsType,
-					HasXmmsType: len(xmmsType) > 0,
-				})
-
-			}
-			if !skip {
-				functions = append(functions, Function{
-					ObjectId:  objectId + 1,
-					CommandId: commandId + offset,
-					Name:      toCamelCase(obj.Name+"_"+meth.Name, true),
-					Args:      args,
-				})
-			}
+		for commandId, method := range obj.Methods {
+			functions = append(functions, Function{
+				ObjectId:  objectId + 1,
+				CommandId: commandId + offset,
+				Name:      toCamelCase(obj.Name+"_"+method.Name, true),
+				Args:      collectArguments(method.Arguments),
+			})
 		}
 		objectId += 1
 	}
