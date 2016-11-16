94d55f8582cc9b785c913351095308423061dfcb
Daniel Svensson <dsvensson@gmail.com>
2016-11-16 20:01:44 +0100

    Add auto.go dummy that generates the API.

---
+++ xmmsclient/auto.go
@@ -0,0 +1,5 @@
+package xmmsclient
+
+//go:generate go run ../genipc/enums.go ../genipc/functions.go ../genipc/genipc.go ../genipc/xml.go ../data/ipc.xml enums auto_consts.go
+
+//go:generate go run ../genipc/enums.go ../genipc/functions.go ../genipc/genipc.go ../genipc/xml.go ../data/ipc.xml methods auto_commands.go
