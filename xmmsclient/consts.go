3e05756be24c9df711fdbd234f9a06e0f7390fda
Daniel Svensson <dsvensson@gmail.com>
2016-11-16 19:59:02 +0100

    Remove auto-generated constants.

--- xmmsclient/consts.go
+++ xmmsclient/consts.go
@@ -15,23 +15,6 @@ const (
 )
 
 const (
-	ObjectSignal   uint32 = 0
-	ObjectMain     uint32 = 1
-	ObjectPlaylist uint32 = 2
-	ObjectConfig   uint32 = 3
-	ObjectPlayback uint32 = 4
-	ObjectMedialib uint32 = 5
-	/* ... */
-)
-const (
-	CommandMainHello       uint32 = 32
-	CommandMainQuit        uint32 = 33
-	CommandMainListPlugins uint32 = 34
-	CommandMainStats       uint32 = 35
-	/* ... */
-)
-
-const (
 	TypeNone      uint32 = 0
 	TypeError     uint32 = 1
 	TypeInt64     uint32 = 2
@@ -42,25 +25,4 @@ const (
 	TypeDict      uint32 = 7
 	TypeBitBuffer uint32 = 8
 	TypeFloat     uint32 = 9
-)
-
-const (
-	CollectionTypeReference    uint32 = 0
-	CollectionTypeUniverse     uint32 = 1
-	CollectionTypeUnion        uint32 = 2
-	CollectionTypeIntersection uint32 = 3
-	CollectionTypeComplement   uint32 = 4
-	CollectionTypeHas          uint32 = 5
-	CollectionTypeMatch        uint32 = 6
-	CollectionTypeToken        uint32 = 7
-	CollectionTypeEquals       uint32 = 8
-	CollectionTypeNotEqual     uint32 = 9
-	CollectionTypeSmaller      uint32 = 10
-	CollectionTypeSmallerEqual uint32 = 11
-	CollectionTypeGreater      uint32 = 12
-	CollectionTypeGreaterEqual uint32 = 13
-	CollectionTypeOrder        uint32 = 14
-	CollectionTypeLimit        uint32 = 15
-	CollectionTypeMediaset     uint32 = 16
-	CollectionTypeIdlist       uint32 = 17
 )
