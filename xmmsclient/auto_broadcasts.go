7ca88f241be06848989d186dbaf5f2be4358ae76
Daniel Svensson <dsvensson@gmail.com>
2016-12-13 22:43:04 +0100

    Update auto-generated API.

--- xmmsclient/auto_broadcasts.go
+++ xmmsclient/auto_broadcasts.go
@@ -6,118 +6,156 @@ import (
 	"bytes"
 )
 
-type Broadcast struct {
+type IntBroadcast struct {
 	result chan reply
 }
 
-func (b *Broadcast) Next() (XmmsValue, error) {
+type StringBroadcast struct {
+	result chan reply
+}
+
+type DictBroadcast struct {
+	result chan reply
+}
+
+func (b *IntBroadcast) Next() (XmmsInt, error) {
 	__reply := <- b.result
 	if __reply.err != nil {
-		return nil, __reply.err
+		return -1, __reply.err
 	}
 	__buffer := bytes.NewBuffer(__reply.payload)
-	return tryDeserialize(__buffer)
+	__value, __err := tryDeserialize(__buffer)
+	if __err != nil {
+		return -1, __err
+	}
+	return __value.(XmmsInt), nil
+}
+
+func (b *StringBroadcast) Next() (XmmsString, error) {
+	__reply := <- b.result
+	if __reply.err != nil {
+		return "", __reply.err
+	}
+	__buffer := bytes.NewBuffer(__reply.payload)
+	__value, __err := tryDeserialize(__buffer)
+	if __err != nil {
+		return "", __err
+	}
+	return __value.(XmmsString), nil
+}
+
+func (b *DictBroadcast) Next() (XmmsDict, error) {
+	__reply := <- b.result
+	if __reply.err != nil {
+		return XmmsDict{}, __reply.err
+	}
+	__buffer := bytes.NewBuffer(__reply.payload)
+	__value, __err := tryDeserialize(__buffer)
+	if __err != nil {
+		return XmmsDict{}, __err
+	}
+	return __value.(XmmsDict), nil
 }
 
 
 // This broadcast is triggered when the daemon is shutting down.
-func (c *Client) BroadcastMainQuit() Broadcast {
+func (c *Client) BroadcastMainQuit() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(0)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when the playlist changes.
-func (c *Client) BroadcastPlaylistChanged() Broadcast {
+func (c *Client) BroadcastPlaylistChanged() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(1)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is triggered when the position in the playlist changes.
-func (c *Client) BroadcastPlaylistCurrentPos() Broadcast {
+func (c *Client) BroadcastPlaylistCurrentPos() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(2)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is triggered when another playlist is loaded.
-func (c *Client) BroadcastPlaylistLoaded() Broadcast {
+func (c *Client) BroadcastPlaylistLoaded() StringBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(3)})
-	return Broadcast{__chan}
+	return StringBroadcast{__chan}
 }
 
 // This broadcast is triggered when the value of any config property changes.
-func (c *Client) BroadcastConfigValueChanged() Broadcast {
+func (c *Client) BroadcastConfigValueChanged() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(4)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is triggered when the playback status changes.
-func (c *Client) BroadcastPlaybackStatus() Broadcast {
+func (c *Client) BroadcastPlaybackStatus() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(5)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when the playback volume changes.
-func (c *Client) BroadcastPlaybackVolumeChanged() Broadcast {
+func (c *Client) BroadcastPlaybackVolumeChanged() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(6)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is triggered when the played song's media ID changes.
-func (c *Client) BroadcastPlaybackCurrentId() Broadcast {
+func (c *Client) BroadcastPlaybackCurrentId() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(7)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when an entry is added to the medialib.
-func (c *Client) BroadcastMedialibEntryAdded() Broadcast {
+func (c *Client) BroadcastMedialibEntryAdded() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(9)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when the properties of a medialib entry are changed.
-func (c *Client) BroadcastMedialibEntryChanged() Broadcast {
+func (c *Client) BroadcastMedialibEntryChanged() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(10)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when a medialib entry is removed.
-func (c *Client) BroadcastMedialibEntryRemoved() Broadcast {
+func (c *Client) BroadcastMedialibEntryRemoved() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(11)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is triggered when a collection is changed.
-func (c *Client) BroadcastCollectionChanged() Broadcast {
+func (c *Client) BroadcastCollectionChanged() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(12)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is triggered when the status of the mediainfo reader changes.
-func (c *Client) BroadcastMediainfoReaderStatus() Broadcast {
+func (c *Client) BroadcastMediainfoReaderStatus() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(13)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast carries client-to-client messages.
-func (c *Client) BroadcastCourierMessage() Broadcast {
+func (c *Client) BroadcastCourierMessage() DictBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(15)})
-	return Broadcast{__chan}
+	return DictBroadcast{__chan}
 }
 
 // This broadcast is emitted when a client's services are ready.
-func (c *Client) BroadcastCourierReady() Broadcast {
+func (c *Client) BroadcastCourierReady() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(16)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is emitted when a new client connects.
-func (c *Client) BroadcastIpcManagerClientConnected() Broadcast {
+func (c *Client) BroadcastIpcManagerClientConnected() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(17)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
 
 // This broadcast is emitted when a client disconnects.
-func (c *Client) BroadcastIpcManagerClientDisconnected() Broadcast {
+func (c *Client) BroadcastIpcManagerClientDisconnected() IntBroadcast {
 	__chan := c.dispatch(0, 33, XmmsList{XmmsInt(18)})
-	return Broadcast{__chan}
+	return IntBroadcast{__chan}
 }
